7b82c1e1cab964cf5b34d40840e5385b3eb0848a
yuraisme <yuraisme@mail.ru>
2025-01-17 10:42:50 +0300

    fix some func out

--- tuya_cloud.py
+++ tuya_cloud.py
@@ -1,8 +1,11 @@
 import os
 from typing import NamedTuple
+
 import tinytuya
 from dotenv import load_dotenv
+
 from exceptions import APITuyaException
+
 # Turn on Debug Mode
 # tinytuya.set_debug(True)
 
@@ -11,17 +14,20 @@ load_dotenv()
 
 Celcius = float
 Percent = float
- 
+
+
 class SensorData(NamedTuple):
     temperature: Celcius
     humidity: Percent
     battery: int
 
+
 c = tinytuya.Cloud(
-        apiRegion="eu", 
-        apiKey =  os.getenv("API_KEY"),
-        apiSecret=  os.getenv("API_SECRET"),
-        apiDeviceID=os.getenv("TEMP_ID"))
+    apiRegion="eu",
+    apiKey=os.getenv("API_KEY"),
+    apiSecret=os.getenv("API_SECRET"),
+    apiDeviceID=os.getenv("TEMP_ID"),
+)
 
 # Display list of devices
 
@@ -30,16 +36,15 @@ def get_temp():
     result = c.getstatus(os.getenv("TEMP_ID"))
     if result and isinstance(result, dict):
         # for item in result['result']:
-            # print(item['code'],item['value'])
+        # print(item['code'],item['value'])
         return SensorData(
-            temperature=result['result'][0]['value'],
-            humidity=result['result'][1]['value'],
-            battery=result['result'][2]['value'],
+            temperature=result["result"][0]["value"] / 10,
+            humidity=result["result"][1]["value"] / 10,
+            battery=result["result"][2]["value"],
         )
     else:
         raise APITuyaException
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     print(get_temp())
