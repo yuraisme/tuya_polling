f921aa853b6592c2f8254992231e6380d2ebc191
yuraisme <yuraisme@mail.ru>
2025-01-22 01:02:29 +0300

    add pydantic check input data

--- tuya_cloud.py
+++ tuya_cloud.py
@@ -1,8 +1,11 @@
 import os
 from typing import NamedTuple
 
+import pydantic
+import pydantic.error_wrappers
 import tinytuya
 from dotenv import load_dotenv
+from pydantic import BaseModel, model_validator
 
 from exceptions import APITuyaException
 
@@ -14,6 +17,27 @@ load_dotenv()
 
 Celcius = float
 Percent = float
+
+
+class Answer_dict(BaseModel):
+    code: str = "code"
+    value: int
+
+
+class ResponseModel(BaseModel):
+    data: dict[str, int]
+
+    @model_validator(mode="before")
+    def transform_result_to_dict(cls, values):
+        # Преобразование списка result в словарь
+        result_list = values.get("result", [])
+        if result_list != []:
+            values["data"] = {
+                item["code"]: item["value"] for item in result_list
+            }
+            return values
+        else:
+            raise APITuyaException
 
 
 class SensorData(NamedTuple):
@@ -33,17 +57,17 @@ c = tinytuya.Cloud(
 
 
 def get_temp():
-    result = c.getstatus(os.getenv("TEMP_ID"))
-    if result and isinstance(result, dict):
-        # for item in result['result']:
-        # print(item['code'],item['value'])
-        return SensorData(
-            temperature=result["result"][0]["value"] / 10,
-            humidity=result["result"][1]["value"] / 10,
-            battery=result["result"][2]["value"],
-        )
-    else:
+    response = c.getstatus(os.getenv("TEMP_ID")) or {}
+    # print(response)
+    try:
+        result = ResponseModel(**response)
+    except pydantic.error_wrappers.ValidationError:
         raise APITuyaException
+    return SensorData(
+        temperature=result.data["temp_current"] / 10,
+        humidity=result.data["humidity_value"] / 10,
+        battery=result.data["battery_percentage"],
+    )
 
 
 if __name__ == "__main__":
