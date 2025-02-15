9c915fa16fcf30f74bc833a46f0038fa66bcd1f3
yuraisme <yuraisme@mail.ru>
2025-02-15 16:32:17 +0300

    added some doc string

--- tuya_cloud.py
+++ tuya_cloud.py
@@ -1,4 +1,5 @@
 import os
+from logging import INFO, Logger
 from typing import NamedTuple
 
 import pydantic
@@ -14,12 +15,20 @@ from exceptions import APITuyaException
 
 # Connect to Tuya Cloud
 load_dotenv()
+logger = Logger(__name__, level=INFO)
 
 Celcius = float
 Percent = float
 
 
 class SensorData(NamedTuple):
+    """Data structure:
+
+    temperature: Celcius (float)
+    humidity: Percent (float)
+    battery (% of charge): int
+    """
+
     temperature: Celcius
     humidity: Percent
     battery: int
@@ -31,17 +40,28 @@ class list_dict(BaseModel):
 
 
 class MainModel(BaseModel):
+    """
+    Base model class for Tuya JSON like:
+
+    [
+        {'code': 'temp_current', 'value': 238},
+        {'code': 'humidity_value', 'value': 204},
+        {'code': 'battery_percentage', 'value': 100},
+    ]
+    """
+
+    # For extract values from the list of dicts
     result: list[list_dict]
 
     @computed_field
     @property
     def temperature(self) -> float:
-        return self.result[0].value / 10
+        return round(self.result[0].value / 10, 1)
 
     @computed_field
     @property
     def humidity(self) -> float:
-        return self.result[1].value / 10
+        return round(self.result[1].value / 10, 1)
 
     @computed_field
     @property
@@ -56,22 +76,33 @@ c = tinytuya.Cloud(
     apiDeviceID=os.getenv("TEMP_ID"),
 )
 
+
 # Display list of devices
+def get_temp() -> SensorData | None:
+    """
 
+    return temperature, humadity, accum charge from
 
-def get_temp():
+    tuya device
+
+    """
     response = c.getstatus(os.getenv("TEMP_ID")) or {}
-    # print(response)
-    try:
-        # result = ResponseModel(**response)
-        request_result = MainModel(**response)
-    except pydantic.error_wrappers.ValidationError:
+    if response.get("success"):
+        logger.info("Tuya service got access")
+        # print(response)
+        try:
+            # result = ResponseModel(**response)
+            request_result = MainModel(**response)
+        except pydantic.error_wrappers.ValidationError:
+            raise APITuyaException
+        return SensorData(
+            temperature=request_result.temperature,
+            humidity=request_result.humidity,
+            battery=request_result.battery_status,
+        )
+    else:
+        logger.error("Not permission from tuya")
         raise APITuyaException
-    return SensorData(
-        temperature=request_result.temperature,
-        humidity=request_result.humidity,
-        battery=request_result.battery_status,
-    )
 
 
 if __name__ == "__main__":
